da0ceb8e81b60d5eb82bb65134c278b539cdb0b7
augtal <augustas.talacka@gmail.com>
2021-03-08 17:39:32 +0200

    Added methods to extract aditional required info

--- app/Models/Advertisement.php
+++ app/Models/Advertisement.php
@@ -7,9 +7,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Advertisement extends Model
 {
-    public function lastestPrice()
-    {
+    public function lastestPrice(){
         return $this->hasOne(AdvertisementPrices::class)->orderBy('updated_at', 'DESC');
+    }
+
+    public function getCategory(){
+        return $this->hasOne(AdvertCategories::class, 'id', 'category');
+    }
+
+    public function getType(){
+        return $this->hasOne(AdvertTypes::class, 'id', 'type');
+    }
+
+    public function getWebsite(){
+        return $this->hasOne(REWebsites::class, 'id', 'r_e_websites_id');
     }
 
     use HasFactory;
